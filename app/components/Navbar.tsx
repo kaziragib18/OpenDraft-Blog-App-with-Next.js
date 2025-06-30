76da4735a94b79904ed885059ffd4a01cffdd25b
Kazi Md. Ragib <58323531+kaziragib18@users.noreply.github.com>
2025-06-30 22:27:54 +0600

    tailwindCss configuration and localfont initialized

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
     <header className="px-5 py-5 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
         <Link href="/">
-          <Image src="/logo.png" alt="logo" width={144} height={30} />
+          <Image src="/logo.png" alt="logo" width={125} height={25} />
         </Link>
 
         <div className="flex items-center gap-5 text-black">
